8d94c169e5da0ee1838ff957efa2e90b943267c2
Manmeet Kaur Baxi <manmeetkaur0175@gmail.com>
2020-04-06 11:24:35 +0530

    Initial commit

---
+++ CountVowels/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountVowels
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            String word = "Extraordinary";
+            Console.WriteLine(countVowels(word));
+        }
+        public static int countVowels(String str)
+        {
+            String word = str.ToLower();
+            int count = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u')
+                {
+                    count++;
+                }
+            }
+            return count;
+
+        }
+    }
+}
---
+++ NUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f69dc7af-f575-4be0-938d-a3944c06f864")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
